9eec3baaf5018b01192f5e89a2995e46587d99ee
Prince Asante <princeaffumasante@gmail.com>
2024-07-08 12:42:43 +0000

    add permission to token endpoint

--- key_manager/serializers.py
+++ key_manager/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
+from rest_framework.exceptions import PermissionDenied
 
 from .models import AccessKey
 
@@ -29,6 +29,15 @@ class MicroFocusObtainPairSerializer(TokenObtainPairSerializer):
         token = super().get_token(user)
 
         # add custom claims
-        token["is_micro_focus_admin"] = user.groups.filter(name="MicroAdmin").exists()
+        token["is_micro_focus_admin"] = user.is_micro_focus_admin
 
         return token
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        # check if the user is MicroAdmin or superuser
+        if not self.user.is_micro_focus_admin and not self.user.is_superuser:
+            raise PermissionDenied("You do not have permission to obtain a token")
+
+        return data
